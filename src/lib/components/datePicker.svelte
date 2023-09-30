231cf90f3a0ea1627b9a27bed2a1b1a15d425698
Jirapat <bukharney@gmail.com>
2023-10-01 00:36:40 +0700

    add datePicker

---
+++ src/lib/components/datePicker.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let label = 'Label';
+	export let date = new Date().toISOString().slice(0, 10);
+</script>
+
+<div class="datePicker">
+	<label for="date">{label}</label>
+	<input
+		type="date"
+		id="date"
+		bind:value={date}
+		on:change={() => console.log(date)}
+		max={new Date().toISOString().slice(0, 10)}
+		min="2000-01-01"
+	/>
+</div>
