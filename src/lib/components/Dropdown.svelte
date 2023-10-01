c2c70e604a3cf06f8e11884a5e34c3d9b726ab14
Nontawat <mix_sk139@hotmail.com>
2023-10-01 21:39:08 +0700

    feat: add Dropdown
    Co-authored-by: R. Lugsami <RONLUG@users.noreply.github.com>

---
+++ src/lib/components/Dropdown.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let choices: {
+        value: string,
+        label: string,
+    }[];
+</script>
+
+<select {...$$restProps}>
+    {#each choices as choice}
+        <option value={choice.value} label={choice.label}>
+            {choice.label}
+        </option>
+    {/each}
+</select>
