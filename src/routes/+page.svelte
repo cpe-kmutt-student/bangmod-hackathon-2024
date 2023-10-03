5c53d44f48714fd3870e157037fe954b91a3fe3b
Promchai Chooseang <promchai2sin@gmail.com>
2023-10-03 19:30:34 +0700

    feat: add eslint-plugin-tailwindcss and reformat code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,20 @@
+<script lang="ts">
+    import FileInput from "$lib/components/FileInput.svelte";
+
+    let files: FileList;
+</script>
+
 <div class="flex h-screen text-center dark:bg-slate-800">
-	<div class="m-auto">
-		<h1 class="text-4xl font-bold text-slate-800 dark:text-slate-200">Bangmod Hackathon 2024</h1>
-		<p class="text-lg font-normal text-slate-500 dark:text-slate-400 sm:px-16 lg:text-xl xl:px-48">
-			Welcome to Bangmod Hackathon 2024
-		</p>
-	</div>
+    <div class="m-auto">
+        <h1 class="text-4xl font-bold text-slate-800 dark:text-slate-200">Bangmod Hackathon 2024</h1>
+        <p class="text-lg font-normal text-slate-500 dark:text-slate-400 sm:px-16 lg:text-xl xl:px-48">
+            Welcome to Bangmod Hackathon 2024
+        </p>
+        <FileInput bind:files/>
+        {#if files}
+            {#each files as file}
+                <p class="text-slate-800 dark:text-slate-200">{file.name}</p>
+            {/each}
+        {/if}
+    </div>
 </div>
