2b9bb08147491cd3d64a09e8ccec55ecb0ec50a4
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-18 15:23:29 +0700

    style: add tailwind config (#11)
    * add tailwind color
    
    * feat: add custom font to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			background: "#FEFCFC",
+			content: "#0D0D02",
+			second: "#D2D2D7",
+			"extra-blue": "#265BF6",
+			"extra-red": "#AC001A",
+			"extra-orange": "#FB7500",
+			"extra-brown": "#3A230C",
+			"extra-yellow": "#FEBB28",
+			"extra-gold": "#C89601",
+			"extra-ivory": "#F9F6E8",
+		},
+		fontFamily: {
+			"thai": ["Kanit", "sans-serif"],
+			"english": ["Outfit", "sans-serif"],
+			"decor-1": ["Homemade Apple", "cursive"],
+			"decor-2": ["Old Standard TT", "serif"],
+		},
 		extend: {}
 	},
 	plugins: []
