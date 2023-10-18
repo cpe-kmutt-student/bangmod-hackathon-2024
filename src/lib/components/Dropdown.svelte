1e87c41d7be616a05650b526c9f825c69e68e81b
nontaxim <89908219+nontaxim@users.noreply.github.com>
2023-10-18 14:00:41 +0700

    feat: add Dropdown (#4)
    * feat: add Dropdown
    
    Co-authored-by: R. Lugsami <RONLUG@users.noreply.github.com>
    
    * feat: add some attribute on dropdown
    
    ---------
    
    Co-authored-by: R. Lugsami <RONLUG@users.noreply.github.com>

---
+++ src/lib/components/Dropdown.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let choices: {
+        value: string,
+        label: string,
+    }[];
+</script>
+
+<select {...$$restProps} on:change on:focus on:blur on:click on:invalid>
+    {#each choices as choice}
+        <option value={choice.value} label={choice.label}>
+            {choice.label}
+        </option>
+    {/each}
+</select>
