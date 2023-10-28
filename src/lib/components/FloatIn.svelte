baf8cfd3b6f8744e7f260946e004c63ea2889ede
RONLUG <ronnakorn.lugsamipichate@gmail.com>
2023-10-28 22:37:12 +0700

    Add FloatIn.svelte

---
+++ src/lib/components/FloatIn.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { twMerge } from 'tailwind-merge';
+
+    let clazz = ''
+    export {clazz as class}
+
+    let words: Array<string>
+    function animate(node: HTMLElement)
+    {
+        words = node.innerHTML.split(" ")
+        console.log(words)
+    }
+
+</script>
+
+<div class="hidden" use:animate><slot/></div>
+
+<div class={clazz}>
+    {#if words}
+        {#each words as word}
+         <span>{word} </span>
+        {/each}
+    {/if}
+</div>
+
