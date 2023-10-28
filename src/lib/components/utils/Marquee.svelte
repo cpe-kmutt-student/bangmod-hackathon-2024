66f0193f154a56ff7ab9ed28e320f9f874814ae0
Promchai Chooseang <promchai2sin@gmail.com>
2023-10-28 22:22:53 +0700

    fix: Fix marquee bug when resize screen smaller

--- src/lib/components/utils/Marquee.svelte
+++ src/lib/components/utils/Marquee.svelte
@@ -12,10 +12,10 @@
 
 	$: repeat = Math.floor(marqueeWidth / 417) || 1;
 </script>
+<svelte:window bind:innerWidth={marqueeWidth}/>
 <div class={twMerge('flex gap-4 overflow-hidden select-none max-w-full', $$props.class)} role='banner'>
 	{#if ready}
 		<div
-			bind:clientWidth={marqueeWidth}
 			class="shrink-0 flex items-center justify-around min-w-full gap-4 animate-marquee"
 		>
 			<!--	eslint-disable-next-line @typescript-eslint/no-unused-vars-->
