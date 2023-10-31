e585605d5817d8a0979f0a9d33f601e82300d88f
Promchai Chooseang <promchai2sin@gmail.com>
2023-10-31 09:36:49 +0700

    fix: change send mail to not establish the connection first

--- src/lib/server/sendEmail.ts
+++ src/lib/server/sendEmail.ts
@@ -1,4 +1,4 @@
-import * as nodemailer from 'nodemailer';
+import { createTransport } from 'nodemailer';
 
 import { GOOGLE_EMAIL, GOOGLE_EMAIL_PASSWORD } from '$env/static/private';
 
@@ -8,23 +8,23 @@ type MailOption = {
 	html: string;
 };
 
-const transporter = nodemailer.createTransport({
-	service: 'gmail',
-	host: 'smtp.gmail.com',
-	port: 587,
-	secure: false,
-	auth: {
-		user: GOOGLE_EMAIL,
-		pass: GOOGLE_EMAIL_PASSWORD
-	}
-});
-
-transporter.verify((error) => {
-	if (error) console.error(error);
-});
-
 const sendEmail = async (mailOption: MailOption) => {
 	try {
+		const transporter = createTransport({
+			service: 'gmail',
+			host: 'smtp.gmail.com',
+			port: 587,
+			secure: false,
+			auth: {
+				user: GOOGLE_EMAIL,
+				pass: GOOGLE_EMAIL_PASSWORD
+			}
+		});
+
+		transporter.verify((error) => {
+			if (error) console.error(error);
+		});
+
 		await transporter.sendMail({
 			...mailOption,
 			from: `Bangmod Hackathon 2024 <${GOOGLE_EMAIL}>`
